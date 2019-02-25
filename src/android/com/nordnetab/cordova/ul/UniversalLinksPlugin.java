56cf6fd3b4e91f52cc3b45bd0728764fccbe2c8f
tehmaestro <alexnegrila89@gmail.com>
2019-02-25 10:00:09 +0200

    fix(): Fix incorrect log tag (#8)

--- src/android/com/nordnetab/cordova/ul/UniversalLinksPlugin.java
+++ src/android/com/nordnetab/cordova/ul/UniversalLinksPlugin.java
@@ -182,7 +182,7 @@ public class UniversalLinksPlugin extends CordovaPlugin {
 
         // if app was not launched by the url - ignore
         if (!Intent.ACTION_VIEW.equals(action) || launchUri == null) {
-            Log.d("app was not launched by the url: ", action);
+            Log.d("UniversalLinks", "App was not launched by the url: " + action);
             return;
         }
 
